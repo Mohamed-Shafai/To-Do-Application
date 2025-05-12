577464df647f70451f3bdc354b5a21654249e844
Mohamed-Shafai <mohamedshafai75@gmail.com>
2025-05-13 01:45:31 +0300

    Initial Commit

---
+++ TodoApplication/TodoApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApplicationApp.swift
+//  TodoApplication
+//
+//  Created by Mohamed Shafai on 13/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
